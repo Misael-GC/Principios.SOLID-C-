2c09131ea5606f828a7fab23c75ec273fff7a867
Misael-GC <misael.gomez1623@gmail.com>
2023-11-13 22:50:10 -0600

    Aplicando el principio abierto/cerrado

--- 2-OpenClose/EmployeeContractor.cs
+++ 2-OpenClose/EmployeeContractor.cs
@@ -1,15 +1,14 @@
 namespace OpenClose
 {
-    public class EmployeePartTime : Employee
+    public class EmployeeContractor : Employee
     {
 
-        public EmployeePartTime(string fullname, int hoursWorked)
+        public EmployeeContractor(string fullname, int hoursWorked)
         {
             Fullname = fullname;
             HoursWorked = hoursWorked;
-        }        
-        public override decimal CalculateSalaryMonth()
-        {
+        }
+        public override decimal CalculateSalaryMonth(){
             decimal hourValue = 20000M;
             decimal salary = hourValue * HoursWorked;
             return salary;
