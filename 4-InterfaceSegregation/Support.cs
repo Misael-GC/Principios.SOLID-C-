cbf7c2954834db785772190845eb3bfc2444ee88
Misael-GC <misael.gomez1623@gmail.com>
2023-12-23 09:41:16 -0600

    Ejemplos de 4-IterfaceSegregation

---
+++ 4-InterfaceSegregation/Support.cs
@@ -0,0 +1,17 @@
+namespace InterfaceSegregation
+{
+    public class Support : ISupportActivities
+    {
+
+        public void FixMachines()
+        {
+            Console.WriteLine("I'm fix computers");
+        }
+
+        public void ckeckNetworks()
+        {
+            Console.WriteLine("I'm check networks");
+        }
+
+    }
+}
